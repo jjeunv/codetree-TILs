51bfb8aab5b05b1415fdcc808a1b770a5f38c738
jjeunv <124152742+jjeunv@users.noreply.github.com>
2023-11-11 10:33:32 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231111/T를 초과하는 연속 부분 수열/subsequence-above-t.py
@@ -0,0 +1,17 @@
+n,t=map(int,input().split())
+
+num=list(map(int,input().split()))
+
+cnt,maxnum=0,0
+
+for i in range(n):
+    if i==0 or num[i]<=t:
+        cnt=0
+    else:
+        cnt+=1
+    maxnum=max(maxnum,cnt)
+
+if maxnum<=1:
+    print(0)
+else:
+    print(maxnum)
