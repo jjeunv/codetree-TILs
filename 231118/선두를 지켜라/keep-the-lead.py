6d26b765b2febe597a57063baa268f90960c4550
jjeunv <124152742+jjeunv@users.noreply.github.com>
2023-11-18 04:00:24 +0900

    Title: 선두를 지켜라, Time: 160ms, Memory: 40MB, Status: Passed - CodeTree

---
+++ 231118/선두를 지켜라/keep-the-lead.py
@@ -0,0 +1,38 @@
+MAX_T=1000000
+
+A,B=[0]*(MAX_T+1),[0]*(MAX_T+1)
+
+N,M=map(int,input().split())
+
+curA=1
+for _ in range(N):
+    v,t=map(int,input().split())
+    for _ in range(t):
+        A[curA]=A[curA-1]+v
+        curA+=1
+
+curB=1
+for _ in range(M):
+    v,t=map(int,input().split())
+    for _ in range(t):
+        B[curB]=B[curB-1]+v
+        curB+=1
+
+head=0
+cur=0
+ans=0
+for i in range(1,MAX_T):
+    if A[i]>B[i]:
+        cur=1
+    elif A[i]<B[i]:
+        cur=-1 
+    else:
+        continue
+    if head!=cur:
+        ans+=1 
+    head=cur
+
+if ans!=0:
+    print(ans-1)
+else:
+    print(0)
