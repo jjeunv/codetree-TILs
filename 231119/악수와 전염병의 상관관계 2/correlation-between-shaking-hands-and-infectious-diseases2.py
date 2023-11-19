7af8757f68d41edf41af90be93b6f47b23577f12
jjeunv <124152742+jjeunv@users.noreply.github.com>
2023-11-19 14:20:06 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 110ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 231119/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 231119/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -1,10 +1,9 @@
 N,K,P,T=map(int,input().split())
 
 MAX_T=250*250
-people=[0]*(N+1)
+people=[[0 for j in range(2)] for i in range(N+1)]
 time=[(0,0)]*(MAX_T+1)
-
-people[P]=1
+people[P][0]=1
 
 maxt=0
 for _ in range(T):
@@ -12,12 +11,15 @@ for _ in range(T):
     time[t]=(x,y)
     maxt=max(t,maxt)
 
-for t in range(1,maxt):
+for t in range(1,maxt+1):
     x,y=time[t]
-    if people[x]==1:
-        people[y]=1
-    elif people[y]==1:
-        people[x]=1
+    if people[x][0]==1 and people[x][1]<=K:
+        people[y][0]=1
+        people[x][1]+=1
+    elif people[y][0]==1 and people[y][1]<=K:
+        people[x][0]=1
+        people[y][1]+=1
+
 
 for t in range(1,N+1):
-    print(people[t],end='')
+    print(people[t][0],end='')
