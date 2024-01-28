174bd6f9bf05dfddd1679e0884ec1583f1465b44
jjeunv <124152742+jjeunv@users.noreply.github.com>
2024-01-28 20:56:34 +0900

    Title: 원 모양으로 되어있는 방, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240128/원 모양으로 되어있는 방/a-room-in-a-circle.py
@@ -0,0 +1,12 @@
+import sys
+n=int(input())
+
+num=[int(input()) for i in range(n)]
+
+ans=sys.maxsize
+for i in range(n):
+    dist=0
+    for j in range(i+1,i+5):
+        dist+=num[j%n]*(j-i)
+    ans=min(ans,dist)
+print(ans)
