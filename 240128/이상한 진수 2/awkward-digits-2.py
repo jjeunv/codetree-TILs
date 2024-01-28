b2ad7677ae7cf043320d57e0b05110df5258f618
jjeunv <124152742+jjeunv@users.noreply.github.com>
2024-01-28 20:14:04 +0900

    Title: 이상한 진수 2, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 240128/이상한 진수 2/awkward-digits-2.py
+++ 240128/이상한 진수 2/awkward-digits-2.py
@@ -1,9 +1,15 @@
 a=list(map(int,input()))
+n=len(a)
 
-for i in range(len(a)):
+flag=0
+for i in range(n):
     if a[i]==0:
+        flag=1
         a[i]=1
         break
+    
+if flag==0:
+    a[-1]=0
 
 ans=0
 
