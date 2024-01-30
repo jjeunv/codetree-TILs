8c5d4f2aa2cfee2b0bc15da81c3cca25b49a9a91
jjeunv <124152742+jjeunv@users.noreply.github.com>
2024-01-30 23:20:48 +0900

    Title: 작은 구슬의 이동, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240130/작은 구슬의 이동/small-marble-movement.py
@@ -0,0 +1,25 @@
+dx,dy=[0,1,-1,0],[1,0,0,-1]
+
+mapping={
+    'R':0,
+    'D':1,
+    'U':2,
+    'L':3
+}
+
+def in_range(x,y):
+    return 0<x<=n and 0<y<=n
+
+n,t=map(int,input().split())
+x,y,d=input().split()
+x,y=int(x),int(y)
+cur_d=mapping[d]
+
+for _ in range(t):
+    nx,ny=x+dx[cur_d],y+dy[cur_d]
+    if not in_range(nx,ny):
+        cur_d=3-cur_d
+        continue
+    x,y=nx,ny 
+
+print(x,y)
