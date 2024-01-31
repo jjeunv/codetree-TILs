6261e5d5e3e40e41ef1359c35b24fa4e90e5f861
jjeunv <124152742+jjeunv@users.noreply.github.com>
2024-02-01 07:35:31 +0900

    Title: 거울에 레이저 쏘기 2, Time: 243ms, Memory: 49MB, Status: Passed - CodeTree

---
+++ 240201/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -0,0 +1,56 @@
+n=int(input())
+grid=[list(input()) for _ in range(n)]
+k=int(input())
+
+dx,dy=[1,0,-1,0],[0,-1,0,1]
+direction=[0]*(4*n+1)
+
+num=1
+for d in range(4):
+    for i in range(n):
+        direction[num]=d 
+        num+=1
+
+#print(direction)
+def next_direction(m,d):
+    if m=='/':
+        if d==0 or d==2:
+            d+=1
+        elif d==1 or d==3:
+            d-=1
+    else:
+        d=3-d 
+    return d 
+
+def in_range(x,y):
+    return 0<=x<n and 0<=y<n
+
+x,y=0,0
+cur_d=3
+coordinate=[[0 for i in range(2)] for j in range(4*n+1)]
+for i in range(1,4*n+1):
+    coordinate[i][0],coordinate[i][1]=x,y
+    nx,ny=x+dx[cur_d],y+dy[cur_d]
+    if not in_range(nx,ny):
+        cur_d=(cur_d+1)%4
+        continue    
+    x+=dx[cur_d]
+    y+=dy[cur_d]
+#print(coordinate)
+x,y=coordinate[k]
+cur_d=direction[k]
+ans=1
+i=1
+while True:
+    #print(i,cur_d)
+    cur_d=next_direction(grid[x][y],cur_d)
+    #print(x,y,grid[x][y],cur_d)
+    nx,ny=x+dx[cur_d],y+dy[cur_d]
+    #print(i,nx,ny)
+    if not in_range(nx,ny):
+        print(ans)
+        break 
+    x+=dx[cur_d]
+    y+=dy[cur_d]
+    ans+=1
+    i+=1
