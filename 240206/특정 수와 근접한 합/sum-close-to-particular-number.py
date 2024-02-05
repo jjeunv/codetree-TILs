3d09c69024ebeca584baeee8f95ddb891ec2fa41
jjeunv <124152742+jjeunv@users.noreply.github.com>
2024-02-06 03:30:40 +0900

    Title: 특정 수와 근접한 합, Time: 125ms, Memory: 25MB, Status: Passed - CodeTree

--- 240206/특정 수와 근접한 합/sum-close-to-particular-number.py
+++ 240206/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -5,11 +5,9 @@ num=list(map(int,input().split()))
 
 ans=sys.maxsize
 
-for i in range(n-4):
-    for j in range(i+1,n-3):
-        for k in range(j+1,n-2):
-            for l in range(k+1,n):
-                cur=abs(s-(num[i]+num[j]+num[k]+num[l]))
-                ans=min(ans,cur)
+for i in range(n-1):
+    for j in range(i+1,n):
+        cur=abs(s-(sum(num)-num[i]-num[j]))
+        ans=min(ans,cur)
 
 print(ans)
