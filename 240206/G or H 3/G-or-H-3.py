f4f83694744d34a2d3242d34dc5dc0ba6e32663d
jjeunv <124152742+jjeunv@users.noreply.github.com>
2024-02-06 04:57:08 +0900

    Title: G or H 3, Time: 213ms, Memory: 25MB, Status: Passed - CodeTree

--- 240206/G or H 3/G-or-H-3.py
+++ 240206/G or H 3/G-or-H-3.py
@@ -2,17 +2,16 @@ n,k=map(int,input().split())
 
 placed=[0]*10001
 
-maxNum=0
 for _ in range(n):
     num,sign=input().split()
     if sign=='G':
         placed[int(num)]=1
     else:
         placed[int(num)]=2
-    maxNum=max(maxNum,int(num))
+
 
 ans=0
-for i in range(1,maxNum-k+1):
+for i in range(1,10000-k+1):
     val=0
     for j in range(i,i+k+1):
         val+=placed[j]
