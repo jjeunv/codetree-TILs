8d2041b1ac0d825bd388c7ba3f57657fe7570626
jjeunv <124152742+jjeunv@users.noreply.github.com>
2024-02-07 07:18:06 +0900

    Title: 바구니 안의 사탕 2, Time: 131ms, Memory: 25MB, Status: Passed - CodeTree

--- 240207/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240207/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -1,15 +1,19 @@
+MAX_NUM=100
+
 n,k=map(int,input().split())
-arr=[0]*101
+arr=[0]*(MAX_NUM+1)
 
 for _ in range(n):
-    a,b=map(int,input().split())
-    arr[b]=a
+    a,x=map(int,input().split())
+    arr[x]+=a
 
-ans=0
-for c in range(k,100-k):
-    cnt=0
-    for i in range(c-k,c+k+1):
-        cnt+=arr[i]
-    ans=max(ans,cnt)
+max_sum=0
 
-print(ans)
+for i in range(MAX_NUM):
+    cur_sum=0
+    for j in range(i-k,i+k+1):
+        if 0<=j<=MAX_NUM:
+            cur_sum+=arr[j]
+    max_sum=max(max_sum,cur_sum)
+
+print(max_sum)
