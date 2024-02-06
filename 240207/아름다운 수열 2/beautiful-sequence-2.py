5bdfe2f5a619596b2c2967e682f9beff3244a0d1
jjeunv <124152742+jjeunv@users.noreply.github.com>
2024-02-07 01:01:56 +0900

    Title: 아름다운 수열 2, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240207/아름다운 수열 2/beautiful-sequence-2.py
+++ 240207/아름다운 수열 2/beautiful-sequence-2.py
@@ -7,10 +7,10 @@ numM=input().split()
 
 def check(a,b):
     for i in b:
-        if not i in a:
+        if a.count(i) != b.count(i):
             return False
     for i in a:
-        if not i in b:
+        if a.count(i) != b.count(i):
             return False
     return True
 
@@ -20,7 +20,6 @@ for i in range(n-m+1):
     for j in range(i,i+m):
         num.append(numN[j])
     if check(num,numM):
-
         ans+=1
 
 print(ans)
