d7e4f249ab1cff690b7e98aa7100351ef5ba824b
jjeunv <124152742+jjeunv@users.noreply.github.com>
2024-02-07 00:55:57 +0900

    Title: 아름다운 수열 2, Time: 147ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240207/아름다운 수열 2/beautiful-sequence-2.py
@@ -0,0 +1,22 @@
+from itertools import permutations
+
+n,m=map(int,input().split())
+
+numN=input().split()
+numM=input().split()
+
+def check(a,b):
+    for i in b:
+        if not i in a:
+            return False
+    return True
+
+ans=0
+for i in range(n-m+1):
+    num=[]
+    for j in range(i,i+m):
+        num.append(numN[j])
+    if check(num,numM):
+        ans+=1
+
+print(ans)
