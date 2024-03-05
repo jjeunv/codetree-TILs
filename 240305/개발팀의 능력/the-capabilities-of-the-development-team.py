d25824b78ca0fb5f6b661bfaf86103bf77068e7c
jjeunv <124152742+jjeunv@users.noreply.github.com>
2024-03-05 15:54:31 +0900

    Title: 개발팀의 능력, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240305/개발팀의 능력/the-capabilities-of-the-development-team.py
+++ 240305/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -6,9 +6,7 @@ def get_diff(i,j,k,l):
     sum1=num[i]+num[j]
     sum2=num[k]+num[l]
     sum3=sum(num)-sum1-sum2
-    if sum1==sum2==sum3:
-        return sys.maxsize
-    return max([sum1,sum2,sum3])-min([sum1,sum2,sum3])
+    return (sum1,sum2,sum3)
 
 ans=sys.maxsize
 flag=0
@@ -16,15 +14,16 @@ for i in range(5):
     for j in range(i+1,5):
         for k in range(5):
             for l in range(k+1,5):
-                if i==j or i==k or j==k or j==l:
+                if i==l or i==k or j==k or j==l:
                     continue
-                if num[i]==num[j] or num[k]==num[l]:
-
+                sum1,sum2,sum3=get_diff(i,j,k,l)
+                if sum1==sum2==sum3:
                     continue
-                ans=min(ans,get_diff(i,j,k,l))
-                #print(i,j,k,l,get_diff(i,j,k,l))
                 flag=1
-if flag==0:
-    print(-1) 
-else: 
+                ans=min(ans,max([sum1,sum2,sum3])-min([sum1,sum2,sum3]))
+
+
+if flag==1:
     print(ans)
+else:
+    print(-1)
