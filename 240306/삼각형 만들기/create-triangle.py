22afc58e98e85d5b67a6eca8486d053d3e5744d2
jjeunv <124152742+jjeunv@users.noreply.github.com>
2024-03-06 20:11:19 +0900

    Title: 삼각형 만들기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240306/삼각형 만들기/create-triangle.py
+++ 240306/삼각형 만들기/create-triangle.py
@@ -4,39 +4,28 @@ location=[list(map(int,input().split())) for _ in range(n)]
 
 ans=0
 
-def area_x(p1,p2,p3):
+def area(p1,p2,p3):
     return abs(p1[0]-p2[0])*p3[1]
 
-def area_y(p1,p2,p3):
-    return abs(p1[1]-p2[1])*p3[0]
 def check_x(i,j,k):
     p1,p2,p3=location[i],location[j],location[k]
-    if p1[1]==p2[1] :
-        return area_x(p1,p2,p3)
-    if p2[1]==p3[1]:
-        return area_x(p2,p3,p1)
-    if p3[1]==p1[1]:
-        return area_x(p3,p1,p2)      
+    if p1[1]==p2[1] or p2[1]==p3[1] or p3[1]==p1[1]:
+        return True     
     return 0
 
 def check_y(i,j,k):
     p1,p2,p3=location[i],location[j],location[k]
-    if p1[0]==p2[0] :
-        return area_y(p1,p2,p3)
-    if p2[0]==p3[0]:
-        return area_y(p2,p3,p1)
-    if p3[0]==p1[0]:
-        return area_y(p3,p1,p2)      
-    return 0
+    if p1[0]==p2[0] or p2[0]==p3[0] or p3[0]==p1[0]:
+        return True     
+    return False
 
 for i in range(n):
     for j in range(n):
         for k in range(n):
             if i==j or j==k or i==k:
                 continue
-            area1=check_x(i,j,k)
-            area2=check_y(i,j,k)
-            ans=max(ans,max(area1,area2))
+            if check_x(i,j,k) and check_y(i,j,k):
+                ans=max(ans,area(i,j,k))
 
 if ans==0:
     print(0)
