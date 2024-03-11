ef47fdd8998ae82aef75bd2fd1d1ca9d2ff8d222
jjeunv <124152742+jjeunv@users.noreply.github.com>
2024-03-11 21:22:31 +0900

    Title: 스승의 은혜 2, Time: 126ms, Memory: 25MB, Status: Passed - CodeTree

--- 240311/스승의 은혜 2/the-grace-form-teacher-2.py
+++ 240311/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -2,7 +2,7 @@ n,b=map(int,input().split())
 price=[int(input()) for _ in range(n)]
 
 ans=0
-
+price.sort()
 for i in range(n):
     cnt_price=0
     num=0
@@ -12,7 +12,7 @@ for i in range(n):
                 cnt_price+=price[j]//2
                 num+=1
             else:
-                break
+                continue
         else:
             if cnt_price+price[j]<=b:
                 cnt_price+=price[j]
