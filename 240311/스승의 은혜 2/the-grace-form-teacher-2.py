cb404ac2beae1d8b3e81132e0a0a50d004a99ff1
jjeunv <124152742+jjeunv@users.noreply.github.com>
2024-03-11 20:58:41 +0900

    Title: 스승의 은혜 2, Time: 112ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240311/스승의 은혜 2/the-grace-form-teacher-2.py
+++ 240311/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -7,15 +7,19 @@ for i in range(n):
     cnt_price=0
     num=0
     for j in range(n):
-        if cnt_price+price[j]<=b:
-            if i==j:
-                cnt_price+=price[i]//2 
+        if i==j:
+            if cnt_price+price[j]//2<=b:
+                cnt_price+=price[j]//2
+                num+=1
             else:
-                cnt_price+=price[j]
-            num+=1
+                continue
         else:
-            break
-    
+            if cnt_price+price[j]<=b:
+                cnt_price+=price[j]
+                num+=1
+            else:
+                continue
+
     ans=max(ans,num)
 
 print(ans)
