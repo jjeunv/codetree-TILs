6558f6098bc37dd5f7c5a5e67d91fd3488ebdbcd
jjeunv <124152742+jjeunv@users.noreply.github.com>
2024-04-30 19:17:41 +0900

    Title: 방향에 맞춰 이동, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240430/방향에 맞춰 이동/move-in-direction.py
+++ 240430/방향에 맞춰 이동/move-in-direction.py
@@ -5,12 +5,12 @@ for _ in range(int(input())):
     direction,num=input().split()
     for _ in range(int(num)):
         if direction=='E':
-            x,y=x+dx[0],y+dy[0]
+            x+=dx[0]
         elif direction=='S':
-            x,y=x+dx[1],y+dy[1]
+            y+=dy[1]
         elif direction=='W':
-            x,y=x+dx[2],y+dy[2]
+            x+=dx[2]
         else:
-            x,y=x+dx[3],y+dy[3]
+            y+=dy[3]
 
 print(x,y)
