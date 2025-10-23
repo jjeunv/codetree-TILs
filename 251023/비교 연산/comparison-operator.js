333cd4d05aee21e07f16c9982b4780fb10b8e9a5
jjeunv <124152742+jjeunv@users.noreply.github.com>
2025-10-23 23:14:10 +0900

    Title: 비교 연산, Time: 68ms, Memory: 10MB, Status: Passed - Codetree

--- 251023/비교 연산/comparison-operator.js
+++ 251023/비교 연산/comparison-operator.js
@@ -1,12 +1,13 @@
-const fs = require("fs");
-const input = fs.readFileSync(0).toString().trim().split(' ');
+let fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
 
 let a = Number(input[0]);
 let b = Number(input[1]);
 
-console.log(Number(a>=b));
-console.log(Number(a>b));
-console.log(Number(b>=a));
-console.log(Number(b>a));
-console.log(Number(a===b));
-console.log(Number(a!==b));
+
+console.log(a >= b ? 1 : 0);
+console.log(a > b ? 1 : 0);
+console.log(a <= b ? 1 : 0);
+console.log(a < b ? 1 : 0);
+console.log(a == b ? 1 : 0);
+console.log(a != b ? 1 : 0);
