71d641018213a8b1b981560f558a93395289f9c5
jjeunv <124152742+jjeunv@users.noreply.github.com>
2025-10-25 00:58:26 +0900

    Title: a로 채워넣기, Time: 60ms, Memory: 10MB, Status: Runtime Error - Codetree

--- 251025/a로 채워넣기/filling-with-a.js
+++ 251025/a로 채워넣기/filling-with-a.js
@@ -1,9 +1,5 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim();
 
-const fir = input[0];
-const sec = input.slice(2,input.length-2);
-const last = input.slice(input.length-1);
-
-
-console.log(fir+'a'+sec+'a'+last);
+input = input.slice(0,1) +'a' + input.slice(2);
+input = input.slice(0,length-2)+'a'+input.slice(length-1);
