1b1da74cabb0ac037e05e98cc19d77bca51d543f
jjeunv <124152742+jjeunv@users.noreply.github.com>
2025-10-25 00:59:18 +0900

    Title: a로 채워넣기, Time: 63ms, Memory: 10MB, Status: Passed - Codetree

--- 251025/a로 채워넣기/filling-with-a.js
+++ 251025/a로 채워넣기/filling-with-a.js
@@ -2,4 +2,6 @@ const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim();
 
 input = input.slice(0,1) +'a' + input.slice(2);
-input = input.slice(0,length-2)+'a'+input.slice(length-1);
+input = input.slice(0,input.length-2)+'a'+input.slice(input.length-1);
+
+console.log(input);
