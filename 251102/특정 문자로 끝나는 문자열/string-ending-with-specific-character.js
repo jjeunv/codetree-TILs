72352bfdd57e59ffaa9ec77369f8c5e67b87e046
jjeunv <124152742+jjeunv@users.noreply.github.com>
2025-11-02 18:23:00 +0900

    Title: 특정 문자로 끝나는 문자열, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

--- 251102/특정 문자로 끝나는 문자열/string-ending-with-specific-character.js
+++ 251102/특정 문자로 끝나는 문자열/string-ending-with-specific-character.js
@@ -3,9 +3,11 @@ const input = fs.readFileSync(0).toString().trim().split("\n");
 
 const s = input[input.length-1];
 
-
+let flag =0;
 for(let i=0; i<input.length-1; i++){
     if(input[i][input[i].length-1]===s){
+        flag=1;
         console.log(input[i])
     }
 }
+if(flag===0) console.log("None")
