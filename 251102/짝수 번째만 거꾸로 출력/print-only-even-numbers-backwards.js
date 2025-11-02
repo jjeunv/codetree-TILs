d77e8dbc25689e3c8538e1508b7b8c42533283d4
jjeunv <124152742+jjeunv@users.noreply.github.com>
2025-11-02 18:45:05 +0900

    Title: 짝수 번째만 거꾸로 출력, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

--- 251102/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.js
+++ 251102/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.js
@@ -1,9 +1,9 @@
 const fs= require("fs");
-const str = fs.readFileSync(0).toString().trim().split("").reverse().join("");
+const str = fs.readFileSync(0).toString().trim().split("");
 
 let res = "";
 for(let i=0; i<str.length; i++){
-    if(i%2===0) res+=str[i];
+    if(i%2!==0) res+=str[i];
 }
 
-console.log(res)
+console.log(res.split("").reverse().join(""))
