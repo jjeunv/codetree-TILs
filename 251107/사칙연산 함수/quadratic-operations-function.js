51d83d5f9826b6237a4bb6fa60a92c7ec6630dd5
jjeunv <124152742+jjeunv@users.noreply.github.com>
2025-11-07 23:24:58 +0900

    Title: 사칙연산 함수, Time: 71ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 251107/사칙연산 함수/quadratic-operations-function.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+let [a, o, c] = input[0].split(" ");
+a = Number(a);
+c = Number(c);
+// Please Write your code here.
+
+function sum(a,c){
+    return a+c;
+}
+
+function sub(a,c){
+    return a-b;
+}
+
+function mul(a,c){
+    return a*c;
+}
+
+function div(a,c){
+    return parseInt(a/c);
+}
+
+if(o==='+'){
+    console.log(`${a} + ${c} = ${sum(a,c)}`);
+}else if(o==='-'){
+   console.log(`${a} - ${c} = ${sub(a,c)}`);
+}else if(o==='*'){
+   console.log(`${a} * ${c} = ${mul(a,c)}`);
+}else if(o==='/'){
+   console.log(`${a} / ${c} = ${div(a,c)}`);
+}else{
+    console.log('False');
+}
