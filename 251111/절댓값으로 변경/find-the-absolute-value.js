f3601e7d37dc19eb8dde2aecee7f8ebafc37e792
jjeunv <124152742+jjeunv@users.noreply.github.com>
2025-11-11 00:14:13 +0900

    Title: 절댓값으로 변경, Time: 62ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251111/절댓값으로 변경/find-the-absolute-value.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+
+let n = Number(input[0]);
+let arr = input[1].split(" ").map(Number);
+
+// Please Write your code here.
+
+function solve(arr){
+    for(let i=0; i<arr.length; i++){
+        if(arr[i]<0){
+            arr[i]*=-1;
+        }
+    }
+}
+
+solve(arr);
+
+for(let elem of arr){
+    process.stdout.write(elem+" ")
+}
