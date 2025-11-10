0b6048b847e57e01daddb4201114f00542ef566d
jjeunv <124152742+jjeunv@users.noreply.github.com>
2025-11-10 23:46:18 +0900

    Title: 두 정수 값 교환하기, Time: 78ms, Memory: 12MB, Status: Runtime Error - Codetree

---
+++ 251110/두 정수 값 교환하기/to-exchange-two-integer-values.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+let [n, m] = input[0].split(" ").map(Number);
+// Please write your code here.
+
+function swap(a,b){
+    [a,b]=[b,a];
+    return [a,b];
+
+}
+
+let [n,m] = swap(n,m);
+
+console.log(n,m);
+
