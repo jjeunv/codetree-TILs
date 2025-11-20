0466edeb47c05d6bd25d389972873717b6112d4f
jjeunv <124152742+jjeunv@users.noreply.github.com>
2025-11-21 00:23:08 +0900

    Title: 홀수 짝수에 따른 출력값, Time: 62ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251121/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+
+// Please Write your code here.
+
+function solve(n){
+    if(n===1) return 1;
+    if(n===2) return 2;
+    
+    return n+solve(n-2);
+}
+
+console.log(solve(n))
