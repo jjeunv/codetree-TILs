15ed5a89680813c1089f282a0b0bd7dffe74cc8b
jjeunv <124152742+jjeunv@users.noreply.github.com>
2025-11-20 23:52:30 +0900

    Title: 재귀함수를 이용한 별 출력 2, Time: 76ms, Memory: 11MB, Status: Passed - Codetree

---
+++ 251120/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+let n = Number(input[0]);
+// Please Write your code here.
+
+function printNStars(n){
+    if(n===0) return;
+    for(let i=0; i<n; i++){
+        process.stdout.write('* ');
+    }
+    console.log();
+    printNStars(n-1);
+    for(let i=0; i<n; i++){
+        process.stdout.write('* ');
+    }
+    console.log()
+}
+
+printNStars(n);
