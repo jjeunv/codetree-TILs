c27e2414f0b7fb109c5c971bb3cc0345afa5437e
jjeunv <124152742+jjeunv@users.noreply.github.com>
2025-11-23 18:55:24 +0900

    Title: 2개씩 그룹짓기, Time: 64ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251123/2개씩 그룹짓기/group-of-pairs.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const nums = input[1].trim().split(' ').map(Number);
+
+// Please Write your code here.
+
+nums.sort((a,b)=>a-b);
+
+let max = 0;
+
+for(let i=0; i<n; i++){
+    let sum=nums[i]+nums[2*n-i]
+    if(sum>max){
+        max = sum;
+    }
+}
+
+console.log(max);
