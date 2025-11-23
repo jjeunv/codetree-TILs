525f531333d105af8408e9ecf58d0dbf69ea23c8
jjeunv <124152742+jjeunv@users.noreply.github.com>
2025-11-23 18:34:41 +0900

    Title: 단어 정렬, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251123/단어 정렬/sorting-words.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+const words = input.slice(1, n + 1);
+// Please Write your code here.
+
+
+words.sort();
+
+for(let w of words){
+    console.log(w);
+}
