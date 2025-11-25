b7c93b597d7249b5f042f9e63ff40f1f638edc3e
jjeunv <124152742+jjeunv@users.noreply.github.com>
2025-11-26 00:30:14 +0900

    Title: Date to Date, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

--- 251126/Date to Date/date-to-date.js
+++ 251126/Date to Date/date-to-date.js
@@ -7,4 +7,13 @@ const [m1, d1, m2, d2] = input[0].split(' ').map(Number);
 
 const days=[0,31,28,31,30,31,30,31,31,30,31,30,31];
 
-console.log((days[m1]-d1+1)+d2)
+function day(m,d){
+    let sum=0;
+    for(let i=1; i<m; i++){
+        sum+=days[i];
+    }
+    sum+=d;
+    return sum;
+}
+
+console.log(day(m2,d2)-day(m1,d1)+1);
