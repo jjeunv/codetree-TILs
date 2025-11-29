cfba813db1e2043ecec0ebc05dc2ad68f1dc5719
jjeunv <124152742+jjeunv@users.noreply.github.com>
2025-11-29 22:02:33 +0900

    Title: 2진수로 변환하기, Time: 62ms, Memory: 10MB, Status: Passed - Codetree

--- 251129/2진수로 변환하기/convert-to-binary.js
+++ 251129/2진수로 변환하기/convert-to-binary.js
@@ -4,6 +4,7 @@ let n = Number(input[0]);
 // Please Write your code here.
 
 let nums=[];
+
 while(n!=0){
     nums.push(n%2);
     n=Math.floor(n/2);
@@ -11,4 +12,8 @@ while(n!=0){
 
 nums.reverse();
 
+if(nums.length){
 console.log(nums.join(""))
+}else{
+    console.log(0)
+}
