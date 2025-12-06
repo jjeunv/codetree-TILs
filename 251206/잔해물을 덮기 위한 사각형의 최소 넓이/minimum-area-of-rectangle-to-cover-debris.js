ed215508204deddf3a522a5d9944d1d8d5c6f7e3
jjeunv <124152742+jjeunv@users.noreply.github.com>
2025-12-06 23:58:57 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 168ms, Memory: 59MB, Status: Passed - Codetree

--- 251206/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.js
+++ 251206/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.js
@@ -1,56 +1,41 @@
+const OFFSET = 1000;
+const MAX_R = 2000;
+
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
-const rect1 = input[0].split(' ').map(Number);
-const rect2 = input[1].split(' ').map(Number);
+const rects = input.map((line)=>line.split(' ').map(Number));
 
-const OFFSET = 1000;
-const MAX_R = 2001;
+const grid = Array.from({length: MAX_R+1}, ()=>Array(MAX_R+1).fill(0));
 
-const grid = Array.from({length:MAX_R}, ()=>Array(MAX_R).fill(0));
+rects.forEach((rect,index)=>{
+    let [x1,y1,x2,y2] = rect;
+    x1+=OFFSET; y1+=OFFSET;
+    x2+=OFFSET; y2+=OFFSET;
 
-let [x1,y1,x2,y2] = rect1;
-x1+=OFFSET; y1+=OFFSET;
-x2+=OFFSET; y2+=OFFSET;
-for(let i=x1; i<x2; i++){
-    for(let j=y1; j<y2; j++){
-        grid[i][j]=1;
-    }
-}
-
-[x1,y1,x2,y2] = rect2;
-x1+=OFFSET; y1+=OFFSET;
-x2+=OFFSET; y2+=OFFSET;
-for(let i=x1; i<x2; i++){
-    for(let j=y1; j<y2; j++){
-        grid[i][j]=0;
-    }
-}
-
-[x1,y1,x2,y2] = rect1;
-x1+=OFFSET; y1+=OFFSET;
-x2+=OFFSET; y2+=OFFSET;
-
-let c = 0;
-for(let i=x1; i<x2; i++){
-    let cnt=0
-    for(let j=y1; j<y2; j++){
-        if(grid[i][j]===1){
-            cnt++;
+    for(let x = x1; x<x2; x++){
+        for(let y = y1; y<y2; y++){
+            grid[x][y] = index+1;
         }
     }
-    c = Math.max(c,cnt);
-}
+});
 
-let r = 0 ;
-for(let i=y1; i<y2; i++){
-    let cnt=0
-    for(let j=x1; j<x2; j++){
-        if(grid[j][i]===1){
-            cnt++;
+let flag=false;
+let min_x = MAX_R; max_x = 0; min_y = MAX_R; max_y = 0;
+for(let x=0; x<MAX_R; x++){
+    for(let y =0; y<MAX_R; y++){
+        if(grid[x][y]===1){
+            flag = true;
+            min_x = Math.min(min_x,x);
+            max_x=Math.max(max_x,x);
+            min_y=Math.min(min_y,y);
+            max_y=Math.max(max_y,y);
         }
     }
-    r = Math.max(r,cnt);
 }
 
-console.log(r*c)
+if(!flag){
+    console.log(0);
+}else{
+    console.log((max_x-min_x+1)*(max_y-min_y+1))
+}
