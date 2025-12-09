a6760ebb04457a9949ff55f2d4fff845af54e05b
jjeunv <124152742+jjeunv@users.noreply.github.com>
2025-12-09 22:35:37 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 66ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251209/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [N,K,P,T] = input[0].split(' ').map(Number);
+const shakes = input.slice(1).map((shake)=>shake.split(' ').map(Number));
+
+shakes.sort((a,b)=>a[0]-b[0]);
+const maxT = shakes[T-1][0];
+
+const arr = Array(N+1).fill(0);
+const cnt = Array(N+1).fill(0);
+
+arr[P]=1;
+cnt[P]=K;
+
+for(let shake of shakes){
+    let [t, a, b] = shake;
+    if(cnt[a]!==0 && cnt[b]!==0){
+        cnt[a]--;
+        cnt[b]--;
+    }else if(cnt[a]!==0){
+        cnt[a]--;
+        arr[b]=1;
+        cnt[b]=K;
+    }else if(cnt[b]!==0){
+        cnt[b]--;
+        arr[a]=1;
+        cnt[a]=K;
+    }
+}
+
+arr.shift()
+console.log(arr.join(''))
