3a2d2a56f592b198402af13583ae9c8588924b98
jjeunv <124152742+jjeunv@users.noreply.github.com>
2025-12-11 00:30:52 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

--- 251211/빙빙 돌며 숫자 사각형 채우기/snail-number-square.js
+++ 251211/빙빙 돌며 숫자 사각형 채우기/snail-number-square.js
@@ -1,13 +1,13 @@
 const fs = require("fs");
 const [n,m] = fs.readFileSync(0).toString().trim().split(' ').map(Number)
 
-const arr = Array.from({length: n+1},()=>Array(m+1).fill(0));
+const arr = Array.from({length: n},()=>Array(m).fill(0));
 
 
 const dx = [0,1,0,-1], dy = [1,0,-1,0];
 
 function isValid(x,y){
-    return 0<=x && x<n && 0<=y && y<n;
+    return 0<=x && x<n && 0<=y && y<m;
 }
 
 let x=0, y=0;
