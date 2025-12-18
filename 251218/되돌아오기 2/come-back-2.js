734f157c9fb9c987cf0fde41a3dcfff3133a46ca
jjeunv <124152742+jjeunv@users.noreply.github.com>
2025-12-18 23:56:17 +0900

    Title: 되돌아오기 2, Time: 69ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251218/되돌아오기 2/come-back-2.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const commands = input[0];
+// Please Write your code here.
+
+const dx = [0,1,0,-1], dy=[1,0,-1,0];
+
+let x = 0, y =0;
+let dir = 3;
+let t =0;
+for(let i=0; i<commands.length; i++){
+    const c = commands[i];
+    if(c==='L'){
+        dir = (dir-1+4)%4;
+    }
+    else if(c==='R'){
+        dir = (dir+1)%4;
+    }else if(c==='F'){
+        x+=dx[dir], y+=dy[dir];
+    }
+    t++;
+    if(x===0 && y===0){
+        break;
+    }
+}
+
+console.log(t)
+
