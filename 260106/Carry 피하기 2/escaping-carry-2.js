100731ec919a68149e0395c6a81d75365cb37082
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-01-06 22:56:34 +0900

    Title: Carry 피하기 2, Time: 79ms, Memory: 12MB, Status: Wrong Answer - Codetree

---
+++ 260106/Carry 피하기 2/escaping-carry-2.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const arr = input.slice(1, 1 + n);
+
+// Please Write your code here.
+function carry(arr){
+    const nums = Array.from({length:n}, ()=>Array(0));
+    for(let i=0; i<arr.length; i++){
+        for(let j=0; j<arr[i].length; j++){
+            nums[j].push(Number(arr[i][arr[i].length-j-1]))
+        }
+
+    }
+
+    for(let i=0; i<nums.length; i++){
+        let sum = 0;
+        for(let j=0; j<nums[i].length; j++){
+            sum+=nums[i][j];
+        }
+        if(sum>10){
+            return -1;
+        }
+    }
+
+    let sum = 0;
+    for(let i=0; i<arr.length; i++){
+        sum+=Number(arr[i]);
+    }
+    return sum;
+}
+
+
+let ans = -1;
+
+for(let i=0; i<n; i++){
+    for(let j=i+1; j<n; j++){
+        for(let k=j+1; k<n; k++){
+            ans = Math.max(ans, carry([arr[i], arr[j], arr[k]]))
+        }
+    }
+}
+
+console.log(ans)
