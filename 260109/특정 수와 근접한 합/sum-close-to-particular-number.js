9139b03ceee3324673e9594b8086dbf4c19e5e7a
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-01-09 20:25:35 +0900

    Title: 특정 수와 근접한 합, Time: 66ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260109/특정 수와 근접한 합/sum-close-to-particular-number.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, s] = input[0].split(' ').map(Number);
+const arr = input[1].split(' ').map(Number);
+
+// Please Write your code here.
+
+let sum = 0;
+arr.forEach((n)=>sum+=n);
+
+let ans = sum;
+
+for(let i=0; i<n; i++){
+    for(let j=i+1; j<n; j++){
+        let cnt = sum-(arr[i]+arr[j]);
+        ans = Math.min(ans, cnt>s?cnt-s:s-cnt);
+    }
+}
+
+console.log(ans)
