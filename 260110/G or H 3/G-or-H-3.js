8898ad162649c36851d0a5d0ca644a7d20b2aebf
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-01-10 14:53:57 +0900

    Title: G or H 3, Time: 80ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260110/G or H 3/G-or-H-3.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+const [n, k] = input[0].split(' ').map(Number);
+const people = input.slice(1).map((m)=>m.split(' '));
+
+const arr = Array(10000+1).fill(0);
+
+for(let i=0; i<people.length; i++){
+  const [idx, c] = people[i];
+  arr[Number(idx)] = c==='G'?1:2
+}
+
+let ans = 0;
+for(let i = 1; i<=n; i++){
+  let sum = 0;
+  for(let j=i; j<=i+k; j++){
+    sum+=arr[j];
+  }
+  ans = Math.max(ans, sum);
+}
+
+console.log(ans)
