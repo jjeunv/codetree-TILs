64f27b6568697b85fdda41ab895ca52f3c3d21fc
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-01-10 22:35:53 +0900

    Title: 바구니 안의 사탕 2, Time: 69ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 260110/바구니 안의 사탕 2/candy-in-the-basket-2.js
+++ 260110/바구니 안의 사탕 2/candy-in-the-basket-2.js
@@ -10,12 +10,14 @@ for(let c of candy){
 }
 
 let ans = 0;
-for(let i=1; i<100-2*k; i++){
-    let cnt = 0;
-    for(let j=i; j<=i+2*k; j++){
-        cnt+=arr[j];
+for(let i=1; i<101; i++){
+    let sum = 0;
+    for(let j=i-k; j<=i+k; j++){
+        if(0<=j && j<101){
+            sum+=arr[j];
+        }
     }
-    ans = Math.max(ans,cnt)
+    ans = Math.max(ans, sum)
 }
 
 console.log(ans)
