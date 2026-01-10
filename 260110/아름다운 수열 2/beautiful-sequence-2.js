f55d6211d3517038b66701ccef5d4303daaf17c4
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-01-10 22:08:20 +0900

    Title: 아름다운 수열 2, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

--- 260110/아름다운 수열 2/beautiful-sequence-2.js
+++ 260110/아름다운 수열 2/beautiful-sequence-2.js
@@ -3,48 +3,22 @@ const input = fs.readFileSync(0).toString().trim().split("\n");
 
 const [n, m] = input[0].split(" ").map(Number);
 const arr1 = input[1].split(" ").map(Number);
-const arr2 = input[2].split(" ").map(Number);
+const arr2 = input[2].split(" ").map(Number).sort((a,b)=>a-b);
 
-// 아름다운 수
-const beauties = [];
-for(let i=0; i<m; i++){
-    for(let j=0; j<2; j++){
-        beauties.push([...arr2]);
-        arr2.reverse()
-    }
-    arr2.push(arr2.shift())
-}
-
-function isSame(arr){
-    for(let beauty of beauties){
-        let flag = true;
-        for(let i=0; i<m; i++){
-            if(arr[i]!==beauty[i]){
-                flag = false;
-                break;
-            }
-        }
-        if(flag){
-            return true;
-        }
-    }
-    return false;
-}
-
-let ans = 0;
-
-console.log(beauties);
-console.log('-----------------------------')
+let cnt = 0;
 for(let i=0; i<n-m+1; i++){
-    const curArr = [];
-    for(let j=i; j<i+m; j++){
-        curArr.push(arr1[j]);
+    const tmp = arr1.slice(i,i+m).sort((a,b)=>a-b);
+    let issame = true;
+    for(let j=0; j<m; j++){
+        if(tmp[j]!==arr2[j]){
+            issame= false;
+            break;
+        }
     }
-    if(isSame(curArr)){
-        console.log(curArr)
-        ans++;
+    if(issame){
+        cnt++;
     }
+    
 }
 
-
-console.log(ans)
+console.log(cnt)
