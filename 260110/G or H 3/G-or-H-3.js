de2095846e8a02fce8a5db1fd5c551a9875366c1
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-01-10 15:17:48 +0900

    Title: G or H 3, Time: 75ms, Memory: 10MB, Status: Passed - Codetree

--- 260110/G or H 3/G-or-H-3.js
+++ 260110/G or H 3/G-or-H-3.js
@@ -3,7 +3,7 @@ const input = fs.readFileSync(0).toString().trim().split("\n");
 const [n, k] = input[0].split(' ').map(Number);
 const people = input.slice(1).map((m)=>m.split(' '));
 
-const arr = Array(20000+1).fill(0);
+const arr = Array(10000+1).fill(0);
 
 for(let i=0; i<people.length; i++){
   const [idx, c] = people[i];
@@ -11,9 +11,9 @@ for(let i=0; i<people.length; i++){
 }
 
 let ans = 0;
-for(let i = 1; i<20001-k; i++){
+for(let i = 1; i<10000-k+1; i++){
   let sum = 0;
-  for(let j=i; j<=i+k; j++){
+  for(let j=i; j<i+k+1; j++){
     sum+=arr[j];
   }
   ans = Math.max(ans, sum);
