2a930a308cf5adad422fe661960d6294a259191d
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-01-18 19:57:02 +0900

    Title: 삼각형 만들기, Time: 70ms, Memory: 11MB, Status: Wrong Answer - Codetree

--- 260118/삼각형 만들기/create-triangle.js
+++ 260118/삼각형 만들기/create-triangle.js
@@ -6,22 +6,21 @@ const points = input.slice(1, 1 + n).map(line => line.split(' ').map(Number));
 
 let ans = 0;
 
-for(let i=0; i<n; i++){
-    for(let j=i+1; j<n; j++){
-        for(let k=j+1; k<n; k++){
+for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+        for (let k = j + 1; k < n; k++) {
             let area;
-            const [x1,y1] = points[i], [x2,y2] = points[j], [x3,y3]= points[k];
+            const [x1, y1] = points[i], [x2, y2] = points[j], [x3, y3] = points[k];
 
-            if(y1===y2 && (x1===x3|| x2===x3)){
-                area = Math.abs((x2-x1)*(y3-y1)/2);
+            if (y1 === y2 && (x1 === x3 || x2 === x3)) {
+                area = Math.abs((x2 - x1) * (y3 - y1) / 2);
             }
-            else if(x1===x2 && (y1===y3|| y2===y3)){
-                area = Math.abs((y2-y1)*(x3-x1)/2);
-            }else{
+            else if (x1 === x2 && (y1 === y3 || y2 === y3)) {
+                area = Math.abs((y2 - y1) * (x3 - x1) / 2);
+            } else {
                 continue;
             }
-
-            ans = Math.max(ans,area);
+            ans = Math.max(ans, area);
         }
     }
 }
