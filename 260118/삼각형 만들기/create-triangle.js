cfb43c1b7aefb4a7dae5bed579fa90edb33efb44
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-01-18 20:18:07 +0900

    Title: 삼각형 만들기, Time: 110ms, Memory: 14MB, Status: Passed - Codetree

--- 260118/삼각형 만들기/create-triangle.js
+++ 260118/삼각형 만들기/create-triangle.js
@@ -11,13 +11,13 @@ for (let i = 0; i < n; i++) {
         for (let k = j + 1; k < n; k++) {
             let area;
             const [x1, y1] = points[i], [x2, y2] = points[j], [x3, y3] = points[k];
-
-            if (y1 === y2 && (x1 === x3 || x2 === x3)) {
-                area = Math.abs((x2 - x1) * (y3 - y1));
-            }
-            else if (x1 === x2 && (y1 === y3 || y2 === y3)) {
+            if (x1 === x2 && (y1 === y3 || y2 === y3)) {
                 area = Math.abs((y2 - y1) * (x3 - x1));
-            } else {
+            }else if(x1===x3 && (y2 === y1 || y2 === y3)){
+                area = Math.abs((y1-y3)*(x3-x2));
+            }else if(x2 === x3 && (y1===y2 || y1 === y3)){
+                area = Math.abs((y2-y3)*(x1-x3));
+            }else {
                 continue;
             }
             ans = Math.max(ans, area);
