508fe25d7505f4bc97183ad019beadb31f934093
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-01-21 00:18:03 +0900

    Title: 스승의 은혜 3, Time: 67ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260121/스승의 은혜 3/the-grace-form-teacher-3.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+const [n, b] = input[0].split(' ').map(Number);
+const arr = input.slice(1).map((s)=>s.split(' ').map(Number));
+
+arr.sort((a,b)=>{
+    if(a[0]!==b[0]) return a[0]-b[0];
+    return a[1]-b[1];
+})
+
+let ans = 0;
+
+for(let i=0; i<n; i++){
+    let cost = 0;
+    let cnt = 0;
+    for(let j=0; j<n; j++){
+        if(i===j){
+            cost+=(arr[j][0]/2 + arr[j][1]);
+        }else{
+            cost+=(arr[j][0]+arr[j][1]);
+        }
+        if(cost<=b){
+            cnt++;
+        }else{
+            break;
+        }
+    }
+    ans = Math.max(ans, cnt);
+}
+
+console.log(ans);
