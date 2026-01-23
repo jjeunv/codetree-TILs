39377a1294b468085fd188a0f4e230e39071b4da
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-01-24 03:16:37 +0900

    Title: 훌륭한 점프, Time: 67ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260124/훌륭한 점프/great-jump.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const [n, k] = input[0].split(' ').map(Number);
+const arr = input[1].split(' ').map(Number);
+
+function isValid(num){
+    const cnt = [];
+    for(let i=1; i<n; i++){
+        if(arr[i]<=num){
+            cnt.push(i);
+        }
+    }
+    let cur = 0;
+    for(let i=0; i<cnt.length; i++){
+        if(cnt[i]-cur<=k){
+            cur = cnt[i];
+        }else{
+            return false;
+        }
+    }
+    return true;
+}
+for(let i=1; i<=n; i++){
+    if(isValid(i)){
+        console.log(i);
+        break;
+    }
+}
