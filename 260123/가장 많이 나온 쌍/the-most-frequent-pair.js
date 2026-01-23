41eb1ef6073383f3fa8a578edafcdd48de5351e9
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-01-23 19:43:02 +0900

    Title: 가장 많이 나온 쌍, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

--- 260123/가장 많이 나온 쌍/the-most-frequent-pair.js
+++ 260123/가장 많이 나온 쌍/the-most-frequent-pair.js
@@ -4,8 +4,8 @@ const [n, m] = input[0].split(' ').map(Number);
 const pairs = input.slice(1, 1 + m).map(line => line.split(' ').map(Number));
 
 let ans = 0;
-for(let i=0; i<n; i++){
-    for(let j=0; j<n; j++){
+for(let i=1; i<=n; i++){
+    for(let j=1; j<=n; j++){
         let cnt = 0;
         for(let k=0; k<m; k++){
             const [a,b] = pairs[k];
