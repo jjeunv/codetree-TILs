1751dddeb85694edf0dbafcc7bce5d68383c4635
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-01-29 20:57:18 +0900

    Title: 초기 수열 복원하기, Time: 64ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260129/초기 수열 복원하기/restore-initial-sequence.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const sums = input[1].split(' ').map(Number);
+
+
+let res = 0;
+
+for(let i=1; i<=n; i++){
+    for(let j=1; j<=n; j++){
+        const ans = [];
+        let curNum, startNum;
+        if(i+j===sums[0]){
+            startNum = i;
+            curNum = j;
+        }
+        ans.push(startNum, curNum);
+        for(let k=1; k<sums.length; k++){
+            let num = sums[k]-curNum;
+            if(curNum>0 && curNum<=n){
+                ans.push(num);
+                curNum = num;
+            }else{
+                break;
+            }
+        }
+        if(ans.length===n){
+            res = ans.join(' ');
+        }
+        
+    } 
+}
+
+
+console.log(res)
