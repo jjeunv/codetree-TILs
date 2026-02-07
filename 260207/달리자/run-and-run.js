2f0a18c18463cbe0add9b9664efbc6099c2f1fce
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-02-07 13:36:57 +0900

    Title: 달리자, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260207/달리자/run-and-run.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+const a = input[1].split(' ').map(Number);
+const b = input[2].split(' ').map(Number);
+
+let ans = 0;
+
+for(let i=0; i<n-1; i++){
+    let cnt = a[i]-b[i];
+    ans+=cnt;
+    a[i+1]+=cnt;
+}
+
+console.log(ans)
