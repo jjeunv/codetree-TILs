f7cc2dce335be7e095ce50298abcf72ad3967cfa
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-02-13 14:09:07 +0900

    Title: 코딩톡, Time: 68ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260213/코딩톡/coding-talk.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+const [n, m, p] = input[0].split(' ').map(Number);
+const cmd = input.slice(1).map((c)=>c.split(' '));
+
+const ans = [];
+
+for(let i='A'.charCodeAt(); i<'A'.charCodeAt()+n; i++){
+    let isValid = true;
+    for(let j=p-1; j<n; j++){
+        const [c, cnt] = cmd[j];
+        if(c.charCodeAt()===i){
+            isValid = false;
+            break;
+        }
+    }
+    if(isValid){
+        ans.push(String.fromCharCode(i));
+    }
+}
+
+ans.sort();
+
+console.log(ans.join(' '))
