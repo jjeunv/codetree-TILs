f154be25c96880db2e69f36eff565e4186c7029c
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-02-13 19:09:48 +0900

    Title: 독서실의 거리두기 3, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

--- 260213/독서실의 거리두기 3/study-cafe-keeping-distance-3.js
+++ 260213/독서실의 거리두기 3/study-cafe-keeping-distance-3.js
@@ -4,21 +4,36 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 const seats = input[1].split("").map(Number);
 
-let ans = 0;
-for(let i=1; i<n; i++){
-    if(seats[i]===1) continue;
-    seats[i] = 1;        
-    let distance = Number.MAX_SAFE_INTEGER;
-    for(let j=0; j<n; j++){
-        for(let k=j+1; k<n; k++){
-            if(seats[j]===1 && seats[k]===1){
-                distance = Math.min(distance, k-j);
+let maxDist = 0;
+let [maxI, maxJ] = [-1,-1];
+for(let i=0; i<n; i++){
+    if(seats[i]===1){
+        for(let j=i+1; j<n; j++){
+            if(seats[j]===1){
+                if(j-i>maxDist){
+                    maxDist = j-i;
+                    [maxI, maxJ] = [i, j];
+                }
                 break;
             }
         }
     }
-    ans = Math.max(ans, distance)
-    seats[i] = 0;
 }
+
+const mid = maxI + Math.floor(maxDist/2);
+seats[mid] = 1;
+
+let ans = Number.MAX_SAFE_INTEGER;
+for(let i=0; i<n; i++){
+    if(seats[i]===1){
+        for(let j=i+1; j<n; j++){
+            if(seats[j]===1){
+                ans = Math.min(ans, j-i);
+                break;
+            }
+        }
+    }
+}
+
 
 console.log(ans)
