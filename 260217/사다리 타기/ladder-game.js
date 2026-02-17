85304b8852ef2c4d5c3af6676bcb78cf264e6727
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-02-17 18:11:34 +0900

    Title: 사다리 타기, Time: 141ms, Memory: 15MB, Status: Passed - Codetree

--- 260217/사다리 타기/ladder-game.js
+++ 260217/사다리 타기/ladder-game.js
@@ -1,88 +1,52 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
-const [n, m] = input[0].split(' ').map(Number);
-const lines = input.slice(1, 1 + m).map(line => line.split(' ').map(Number));
+const [n,m] = input[0].split(' ').map(Number);
+const lines = input.slice(1).map((line)=>{
+    const [a,b] = line.split(' ').map(Number);
+    return [b, a-1];
+});
 
-const position = Array(n+1);
-const selectedLine = [];
-let ans = Number.MAX_SAFE_INTEGER;
-const res = Array(n+1);
+lines.sort((a,b)=>a[0]-b[0]);
 
-lines.sort((a,b)=>a[1]-b[1]);
+let selectedLines = [];
 
-// console.log(lines)
-function answer(){
-    for(let i=1; i<=n; i++){
-        res[i] =  [i,0];
-    }
+let ans = m;
 
-    for(let i=1; i<=n; i++){
-        for(let j=0; j<lines.length; j++){
-            const [x, y] = lines[j];
-            if(res[i][1]<=y && res[i][0]===x){
-                res[i][0]+=1;
-                res[i][1]= y;
-            }else if(res[i][1]<=y && res[i][0]===x+1){
-                res[i][0]-=1;
-                res[i][1]=y;
-            }
-        }
-    }
-}
+function possible(){
+    let num1 = Array.from({length:n}, (_,i)=>i);
+    let num2 = Array.from({length:n}, (_,i)=>i);
 
-function ladder(){
-    for(let i=1; i<=n; i++){
-    position[i] = [i,0];
-    }
+    lines.forEach(([_, idx])=>{
+        [num1[idx], num1[idx+1]] = [num1[idx+1], num1[idx]];
+    })
+    selectedLines.forEach(([_, idx])=>{
+        [num2[idx], num2[idx+1]] = [num2[idx+1], num2[idx]];
+    })
 
-    for(let i=1; i<=n; i++){
-        for(let j=0; j<selectedLine.length; j++){
-            const [x, y] = selectedLine[j];
-            if(position[i][1]<=y && position[i][0]===x){
-                position[i][0]+=1;
-                position[i][1]=y;
-            }else if(position[i][1]<=y && position[i][0]===x+1){
-                position[i][0]-=1;
-                position[i][1]=y;
-            }
-        }
-    }
-}
-
-function isValid(){
-    ladder();
-    // console.log(selectedLine)
-    // console.log(position)
-    
-    for(let i=1; i<=n; i++){
-        if(res[i][0]!==position[i][0]){
+    for(let i=0; i<n; i++){
+        if(num1[i]!==num2[i]){
             return false;
         }
     }
+
     return true;
 }
 
-function choose(cnt){
+function findMinLines(cnt){
     if(cnt===m){
-        // console.log(cnt)
-        if(isValid()){
-            // console.log(position)
-            // console.log(selectedLine)
-            ans = Math.min(ans, selectedLine.length);
+        if(possible()){
+            ans = Math.min(ans, selectedLines.length);
         }
         return;
     }
-    
-    selectedLine.push(lines[cnt]);
-    choose(cnt+1);
-    selectedLine.pop();
 
-    choose(cnt+1);
+    selectedLines.push(lines[cnt]);
+    findMinLines(cnt+1);
+    selectedLines.pop();
+
+    findMinLines(cnt+1);
 }
 
-answer();
-choose(0);
+findMinLines(0);
 console.log(ans);
-
-// console.log(res)
