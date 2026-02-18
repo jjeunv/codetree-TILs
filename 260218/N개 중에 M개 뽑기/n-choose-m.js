3be31aab4d5ab2cfa10e565fcf14d140ca0c6c97
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-02-18 19:03:44 +0900

    Title: N개 중에 M개 뽑기, Time: 68ms, Memory: 10MB, Status: Passed - Codetree

--- 260218/N개 중에 M개 뽑기/n-choose-m.js
+++ 260218/N개 중에 M개 뽑기/n-choose-m.js
@@ -1,25 +1,36 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
+// 변수 선언 및 입력
 const [n, m] = input[0].split(' ').map(Number);
+const combination = [];
 
-const arr = [];
+// 방문한 원소들을 출력해줍니다.
+function printCombination() {
+    let answer = '';
+    combination.forEach(elem => {
+        answer += `${elem} `;
+    });
+    console.log(answer.trim());
+}
 
-function choose(cnt, prevNum){
-    if(cnt===m){
-        console.log(arr.join(' '));
+function findCombination(currNum, cnt) {
+    // n개의 숫자를 모두 탐색했으면 더 이상 탐색하지 않습니다.
+    if (currNum === n + 1) {
+        // 탐색하는 과정에서 m개의 숫자를 뽑은 경우 답을 출력해줍니다.
+        if (cnt === m) {
+            printCombination();
+        }
         return;
     }
 
-    for(let i=1; i<=n; i++){
-        if(arr.indexOf(i)!==-1) continue;
-        if(prevNum<i){
-            arr.push(i);
-            choose(cnt+1, i);
-            arr.pop();
-        }
-    }
+    // currNum에 해당하는 숫자를 사용했을 때의 경우를 탐색합니다.
+    combination.push(currNum);
+    findCombination(currNum + 1, cnt + 1);
+    combination.pop();
+
+    // currNum에 해당하는 숫자를 사용하지 않았을 때의 경우를 탐색합니다.
+    findCombination(currNum + 1, cnt);
 }
 
-choose(0,0);
-
+findCombination(1, 0);
