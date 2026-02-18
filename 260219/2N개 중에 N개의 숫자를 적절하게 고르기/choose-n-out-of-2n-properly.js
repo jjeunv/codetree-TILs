75befdaf62c162dc8b58e62b37561d3bd24ca00e
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-02-19 01:09:32 +0900

    Title: 2N개 중에 N개의 숫자를 적절하게 고르기, Time: 63ms, Memory: 10MB, Status: Passed - Codetree

--- 260219/2N개 중에 N개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.js
+++ 260219/2N개 중에 N개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.js
@@ -1,32 +1,42 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
+const INT_MAX = Number.MAX_SAFE_INTEGER;
+
+// 변수 선언 및 입력
 const n = Number(input[0]);
 const num = input[1].split(' ').map(Number);
+const visited = Array(2 * n).fill(false);
 
-let sum=0;
-num.forEach((n)=>sum+=n);
+let ans = INT_MAX;
 
-function calc(num1){
-    let num2 = sum-num1;
-    return Math.abs(num1-num2);
+function calc() {
+    let diff = 0;
+    for (let i = 0; i < 2 * n; i++) {
+        diff = visited[i] ? (diff + num[i]) : (diff - num[i]);
+    }
+    
+    return Math.abs(diff);
 }
 
-let ans = Number.MAX_SAFE_INTEGER;
-
-function solve(idx, cnt, sum){
-    if(cnt===n){
-        // console.log(sum)
-        ans = Math.min(ans, calc(sum));
+function findMin(idx, cnt) {
+    if (cnt === n) {
+        ans = Math.min(ans, calc());
         return;
     }
-
-    if(idx===2*n) return;
-
-    solve(idx+1, cnt+1, sum+num[idx]);
-    solve(idx+1, cnt, sum);
+    
+    if (idx === 2 * n) {
+        return;
+    }
+    
+    // 현재 숫자를 첫 번째 그룹에 사용한 경우입니다.
+    visited[idx] = true;
+    findMin(idx + 1, cnt + 1);
+    visited[idx] = false;
+    
+    // 현재 숫자를 두 번째 그룹에 사용한 경우입니다.
+    findMin(idx + 1, cnt);
 }
 
-solve(0,0 , 0);
-
-console.log(ans)
+findMin(0, 0);
+console.log(ans);
