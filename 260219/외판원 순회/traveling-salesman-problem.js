8e07fb34fbc5415fffe3f26b54e8baae9ce096c2
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-02-19 02:27:49 +0900

    Title: 외판원 순회, Time: 68ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260219/외판원 순회/traveling-salesman-problem.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const cost = input.slice(1, n + 1).map(line => line.split(' ').map(Number));
+
+const visited = Array(n).fill(false);
+
+let ans = Number.MAX_SAFE_INTEGER;
+
+function solve(idx, cnt, value){
+    if(cnt===n-1){
+        ans = Math.min(ans, value+cost[idx][0]);
+        return;
+    }
+
+    for(let i=1; i<4; i++){
+        if(i===idx) continue;
+        if(visited[i]) continue;
+
+        visited[i] = true;
+        solve(i, cnt+1, value+cost[idx][i]);
+
+        visited[i] = false;
+    }
+}
+
+solve(0, 0, 0);
+
+console.log(ans);
