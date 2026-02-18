85be8032886605d0de1c8f1bd7c2dc8ac6e9aa72
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-02-18 14:32:48 +0900

    Title: 방향에 맞춰 최대로 움직이기, Time: 67ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 260218/방향에 맞춰 최대로 움직이기/max-movements-with-direction.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const num = input.slice(1, 1 + n).map(line => line.split(' ').map(Number));
+const moveDir = input.slice(1 + n, 1 + 2 * n).map(line => line.split(' ').map(Number));
+const [r, c] = input[1 + 2 * n].split(' ').map(Number);
+
+const dx = [-1, -1, 0, 1, 1, 1, 0, -1], dy = [0, 1, 1, 1, 0, -1, -1, -1];
+
+function isRange(x,y){
+    return 0<=x && x<n && 0<=y && y<n;
+}
+
+function canGo(x,y,prevNum){
+    return inRange(x,y) && num[x][y]>prevNum;
+}
+const ans = 0;
+function findMax(x, y, cnt){
+    ans = Math.max(ans,cnt);
+
+    const d = moveDir[x][y]-1;
+    for(let i=0; i<n; i++){
+        const nx = x+dx[d]*i, ny = y+dy[d]*i;
+        if(canGo(nx, ny, num[x][y])){
+            findMax(nx, ny, cnt+1);
+        }
+
+    }
+}
+
+findMax(r-1, c-1, 0);
+console.log(ans);
