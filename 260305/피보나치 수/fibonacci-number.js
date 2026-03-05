ac88a98cd202f0b1d2cb63d9c152f597c50c4999
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-03-05 19:01:22 +0900

    Title: 피보나치 수, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260305/피보나치 수/fibonacci-number.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+
+const arr = Array(n+1).fill(-1);
+
+arr[1] = 1;
+arr[2] = 1;
+for(let i=3; i<=n; i++){
+    arr[i] = arr[i-1]+arr[i-2];
+} 
+
+console.log(arr[n])
