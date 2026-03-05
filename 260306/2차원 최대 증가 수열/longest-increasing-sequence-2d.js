b027043f3e96ca8919d56fee9e3c64b3a68df762
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-03-06 02:48:16 +0900

    Title: 2차원 최대 증가 수열, Time: 70ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260306/2차원 최대 증가 수열/longest-increasing-sequence-2d.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, m] = input[0].split(' ').map(Number);
+const grid = input.slice(1, n + 1).map(row => row.split(' ').map(Number));
+
+// Please Write your code here.
+const dp = Array.from({length:n}, ()=>Array(m).fill(-1));
+
+dp[0][0] = 1;
+
+for(let i=0; i<n; i++){
+    for(let j=0; j<n; j++){
+
+        for(let k=0; k<i; k++){
+            for(let l=0; l<j; l++){
+                if(dp[k][l]===-1) continue;
+
+                if(grid[i][j] > grid[k][l]){
+                    dp[i][j] = Math.max(dp[i][j], dp[k][l]+1);
+                }
+            }
+        }
+    }
+}
+
+let ans = -1;
+
+for(let r of dp){
+    ans = Math.max(...r)
+}
+
+console.log(ans)
