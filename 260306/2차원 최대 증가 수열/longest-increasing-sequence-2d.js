95b443b30bc8880704bed46327e132482655a568
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-03-06 02:51:51 +0900

    Title: 2차원 최대 증가 수열, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

--- 260306/2차원 최대 증가 수열/longest-increasing-sequence-2d.js
+++ 260306/2차원 최대 증가 수열/longest-increasing-sequence-2d.js
@@ -10,7 +10,7 @@ const dp = Array.from({length:n}, ()=>Array(m).fill(Number.MIN_SAFE_INTEGER));
 dp[0][0] = 1;
 
 for(let i=0; i<n; i++){
-    for(let j=0; j<n; j++){
+    for(let j=0; j<m; j++){
 
         for(let k=0; k<i; k++){
             for(let l=0; l<j; l++){
