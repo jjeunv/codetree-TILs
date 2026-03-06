ecb5d11c98258ec15b5a80bd44676dea58329863
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-03-06 16:24:25 +0900

    Title: 그래프 탐색, Time: 90ms, Memory: 18MB, Status: Wrong Answer - Codetree

---
+++ 260306/그래프 탐색/graph-traversal.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, m] = input[0].split(' ').map(Number);
+const edges = [];
+for (let i = 1; i <= m; i++) {
+    edges.push(input[i].split(' ').map(Number));
+}
+
+const arr = Array.from({length:n+1}, ()=>Array(n+1).fill(0));
+
+for(let [s,e] of edges){
+    arr[s][e] = 1;
+}
+
+const visited = Array(n+1).fill(false);
+let ans = 0;
+
+function dfs(vertex){
+    for(let i=1; i<=n; i++){
+        if(arr[vertex][i] === 1 && !visited[i]){
+            visited[i] = true;
+            ans+=1;
+            dfs(i);
+        }
+    }
+}
+
+visited[1] = true;
+dfs(1);
+
+console.log(ans)
