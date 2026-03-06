bacba0a3cc9a8200039dde450f8c330ede45e9bd
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-03-06 19:28:35 +0900

    Title: K번 최댓값으로 이동하기, Time: 64ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 260306/K번 최댓값으로 이동하기/move-to-max-k-times.js
+++ 260306/K번 최댓값으로 이동하기/move-to-max-k-times.js
@@ -42,39 +42,91 @@ const [r, c] = input[1 + n].split(' ').map(Number);
 const visited = Array.from({length:n}, ()=>Array(n).fill(false));
 const q = new Queue();
 
-function getPosition(num){
-    let maxNum = 0;
-    let [x,y] = [-1,-1];
-    for(let i=n-1; i>=0; i--){
-        for(let j=n-1; j>=0; j--){
-            if(!visited[i][j] && grid[i][j]<num){
-                if(maxNum<=grid[i][j]){
-                    maxNum = grid[i][j];
-                    [x,y]= [i,j];
-                }
+let currCell = [r-1, c-1];
+
+function isRange(x,y){
+    return 0<=x && x<n && 0<=y && y<n;
+}
+
+function canGo(x,y,targetNum){
+    return isRange(x,y) && !visited[x][y] && grid[x][y]<targetNum;
+}
+
+const initializeVisited = ()=>{
+    for(let i=0; i<n; i++){
+        for(let j=0; j<n; j++){
+            visited[i][j] = false;
+        }
+    }
+}
+
+function bfs(){
+    const dx = [0, 1, 0, -1], dy = [1,0,-1,0];
+
+    let [currX, currY] = currCell;
+    visited[currX][currY] = true;
+    q.push(currCell);
+
+    const targetNum = grid[currX][currY];
+
+    while(!q.empty()){
+        [currX, currY] = q.pop();
+        
+        for(let dir=0; dir<dx.length; dir++){
+            const nx =currX+dx[dir], ny=currY+dy[dir];
+
+            if(canGo(nx,ny,targetNum)){
+                q.push([nx,ny]);
+                visited[nx][ny] = true;
+            }
+        }
+    }
+}
+
+function needUpdate(bestPos, newPos){
+    if(bestPos[0]===-1 && bestPos[1]===-1){
+        return true;
+    }
+
+    const [bestX, bestY] = bestPos;
+    const [newX, newY] = newPos;
+
+    if(grid[bestX][bestY] !== grid[newX][newY]){
+        return grid[newX][newY]>grid[bestX][bestY];
+    }
+    if(newX === bestX){
+        return newY<bestY;
+    }
+    return newX<newY;
+}
+function move(){
+    initializeVisited();
+    
+    bfs(); // 갈 수 있는 위치들 탐색
+
+    let bestPos = [-1,-1];
+    for(let i=0; i<n; i++){
+        for(let j=0; j<n; j++){
+            if(!visited[i][j] || (i===currCell[0] && j===currCell[1])){
+                continue;
+            }
+
+            const newPos = [i,j];
+            if(needUpdate(bestPos, newPos)){
+                bestPos = newPos;
             }
         }
     }
 
-    return [x,y];
 }
+for(let i=0; i<k; i++){
+    const isMoved = move();
 
-function bfs(){
-    while(!q.empty()){
-        if(k===0) break;
-        const [x,y] = q.pop();
-        const [nx,ny] = getPosition(grid[x][y]);
-        if(nx!==-1 && ny!==-1){
-            q.push([nx,ny]);
-            ansX = nx, ansY = ny;
-            k--;
-            visited[nx][ny] = true;
-        }
+    if(!isMoved){
+        break;
     }
 }
 
-let [ansX,ansY] = [r-1,c-1];
-q.push([ansX, ansY]);
-bfs();
 
-console.log(ansX, ansY)
+const [finalX, finalY] = currCell;
+console.log(finalX + 1, finalY + 1);
