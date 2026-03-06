ca5faeaede201da274c7b1b12ba46c6cb32afc4d
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-03-06 16:32:13 +0900

    Title: 그래프 탐색, Time: 98ms, Memory: 14MB, Status: Passed - Codetree

--- 260306/그래프 탐색/graph-traversal.js
+++ 260306/그래프 탐색/graph-traversal.js
@@ -7,26 +7,51 @@ for (let i = 1; i <= m; i++) {
     edges.push(input[i].split(' ').map(Number));
 }
 
-const arr = Array.from({length:n+1}, ()=>Array(n+1).fill(0));
+// const arr = Array.from({length:n+1}, ()=>Array(n+1).fill(0));
 
-for(let [s,e] of edges){
-    arr[s][e] = 1;
-    arr[e][s] = 1;
-}
+// for(let [s,e] of edges){
+//     arr[s][e] = 1;
+//     arr[e][s] = 1;
+// }
 
+// const visited = Array(n+1).fill(false);
+// let ans = 0;
+
+// function dfs(vertex){
+//     for(let i=1; i<=n; i++){
+//         if(arr[vertex][i] === 1 && !visited[i]){
+//             visited[i] = true;
+//             ans+=1;
+//             dfs(i);
+//         }
+//     }
+// }
+
+// visited[1] = true;
+// dfs(1);
+// console.log(ans)
+
+
+const graph = Array.from({length:n+1}, ()=> []);
 const visited = Array(n+1).fill(false);
-let ans = 0;
+let cnt = 0;
 
-function dfs(vertex){
-    for(let i=1; i<=n; i++){
-        if(arr[vertex][i] === 1 && !visited[i]){
-            visited[i] = true;
-            ans+=1;
-            dfs(i);
+function bfs(vertex){
+    graph[vertex].forEach(currV=>{
+        if(!visited[currV]){
+            visited[currV] = true;
+            cnt+=1;
+            bfs(currV);
         }
-    }
+    })
 }
 
-visited[1] = true;
-dfs(1);
-console.log(ans)
+for(let [v1, v2] of edges){
+    graph[v1].push(v2);
+    graph[v2].push(v1);
+}
+
+visited[1]= true;
+bfs(1);
+
+console.log(cnt)
