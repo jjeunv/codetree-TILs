f0f094f060707a1de32aa12dae391457741935d3
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-03-06 16:54:57 +0900

    Title: 마을 구분하기, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260306/마을 구분하기/seperate-village.js
@@ -0,0 +1,55 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const grid = input.slice(1, n + 1).map(line => line.split(' ').map(Number));
+const visited = Array.from({length:n}, ()=>Array(n).fill(false));
+
+function isRange(x,y){
+    return 0<=x && x<n && 0<=y && y<n;
+}
+
+function canGo(x,y){
+    if(!isRange(x,y)) return false;
+
+    if(visited[x][y] || grid[x][y]===0) return false;
+
+    return true;
+}
+
+function dfs(x, y){
+    const dx=[0, -1, 0, 1], dy = [1,0,-1,0];
+
+    for(let dir=0; dir<dx.length; dir++){
+        const nx=x+dx[dir], ny=y+dy[dir];
+        if(canGo(nx, ny)){
+            visited[nx][ny] = true;
+            cnt++;
+            dfs(nx,ny);
+        }
+    }
+}
+
+let town = 0;
+const people = [];
+
+for(let i=0; i<n; i++){
+    for(let j=0; j<n; j++){
+        if(grid[i][j]===1 && visited[i][j]===false){
+            visited[i][j] = true;
+            town+=1;
+            cnt=1;
+            dfs(i, j);
+            people.push(cnt);
+        }
+    }
+}
+
+console.log(town);
+people.sort((a,b)=>a-b);
+for(let p of people){
+    console.log(p);
+}
+
+
+
