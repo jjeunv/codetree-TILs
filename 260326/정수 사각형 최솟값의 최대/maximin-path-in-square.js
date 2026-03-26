a3f76870948bfae3237f4eb274913d14f5cec8a3
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-03-26 12:00:35 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 64ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260326/정수 사각형 최솟값의 최대/maximin-path-in-square.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const grid = input.slice(1, 1 + n).map(line => line.split(' ').map(Number));
+
+const dp = Array.from({ length: n }, () => Array(n).fill(1000000));
+
+for (let i = 1; i < n; i++) {
+    dp[0][i] = Math.min(dp[0][i - 1], grid[0][i]);
+    dp[i][0] = Math.min(dp[i - 1][0], grid[i][0])
+}
+
+for (let i = 1; i < n; i++) {
+    for (let j = 1; j < n; j++) {
+        dp[i][j] = Math.min(Math.max(dp[i - 1][j], dp[i][j - 1]), grid[i][j])
+    }
+}
+
+console.log(dp[n - 1][n - 1]);
