7226fbcc37218067d256d418e3420e9229112da5
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-03-28 14:56:01 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260328/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.js
+++ 260328/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.js
@@ -3,35 +3,36 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 const grid = input.slice(1).map(l => l.split(' ').map(Number));
 
-const dp = Array.from({ length: n }, () => Array(n).fill(1));
+const dp = Array.from({ length: n }, () => Array(n).fill(-1));
 const dx = [0, 1, 0, -1], dy = [1, 0, -1, 0]
 
 function isRange(x, y) {
     return 0 <= x && x < n && 0 <= y && y < n
 }
 
-const pos = [];
-for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-        pos.push([grid[i][j], i, j])
+function findMax(x, y) {
+    if (dp[x][y] !== -1) {
+        return dp[x][y];
     }
-}
 
-pos.sort((a, b) => a[0] - b[0]);
+    let best = 1;
 
-for (let [num, x, y] of pos) {
     for (let dir = 0; dir < 4; dir++) {
         const nx = x + dx[dir], ny = y + dy[dir];
-        if (isRange(nx, ny) && grid[nx][ny] > num) {
-            dp[nx][ny] = Math.max(dp[nx][ny], dp[x][y] + 1);
+        if (isRange(nx, ny) && grid[x][y] < grid[nx][ny]) {
+            best = Math.max(best, findMax(nx, ny) + 1)
+
         }
     }
+    dp[x][y] = best;
+    return best;
 }
 
 let ans = 0;
 for (let i = 0; i < n; i++) {
     for (let j = 0; j < n; j++) {
-        ans = Math.max(ans, dp[i][j])
+
+        ans = Math.max(ans, findMax(i, j))
     }
 }
 
