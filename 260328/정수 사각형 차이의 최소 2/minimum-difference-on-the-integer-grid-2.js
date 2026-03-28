f0725e664701ba78d9882467cbb98c19682b0e29
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-03-28 15:52:02 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260328/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.js
+++ 260328/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.js
@@ -22,6 +22,7 @@ function findMax(minValue) {
         }
     }
 
+    dp[0][0] = grid[0][0];
     for (let i = 1; i < n; i++) {
         dp[0][i] = Math.max(dp[0][i - 1], grid[0][i]);
         dp[i][0] = Math.max(dp[i - 1][0], grid[i][0])
