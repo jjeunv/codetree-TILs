36b72becb66d890ce37a48ae3db4ef3d90f2808b
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-04-02 11:47:26 +0900

    Title: 최대 감소 부분 수열, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260402/최대 감소 부분 수열/longest-decreasing-subsequence.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+const arr = input[1].trim().split(' ').map(Number);
+
+const dp = Array(n).fill(1);
+
+for (let i = 0; i < n; i++) {
+    for (let j = 0; j < i; j++) {
+        if (arr[j] > arr[i]) {
+            dp[i] = Math.max(dp[i], dp[j] + 1)
+        }
+    }
+}
+
+console.log(Math.max(...dp))
