05943cb9968ed9d5fdf68b7f699c9b95c4952d18
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-04-02 15:29:16 +0900

    Title: 알바로 부자 되기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260402/알바로 부자 되기/being-rich-by-working-part-time.js
+++ 260402/알바로 부자 되기/being-rich-by-working-part-time.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
-const arr = input.slice(1).map(l=>l.split(' ').map(Number));
+const arr = input.slice(1).map(l => l.split(' ').map(Number));
 
 // Please Write your code here.
 const dp = Array(n).fill(0);
@@ -10,13 +10,12 @@ for (let i = 0; i < n; i++) {
     dp[i] = arr[i][2];
 }
 
-
 for (let i = 0; i < n; i++) {
     for (let j = 0; j < i; j++) {
         if (arr[j][1] >= arr[i][0]) {
             dp[i] = Math.max(dp[i], dp[j])
         } else if (arr[j][1] < arr[i][0]) {
-            dp[i] = dp[i] + dp[j]
+            dp[i] = Math.max(dp[i], arr[i][2] + dp[j])
         }
     }
 }
