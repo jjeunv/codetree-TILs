1d9a3919159c4a6d1aa888f778b043ea167d0103
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-04-02 11:51:54 +0900

    Title: 최대 점프 횟수, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260402/최대 점프 횟수/maximum-number-of-jumps.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const arr = input[1].trim().split(' ').map(Number);
+
+const dp = Array(n).fill(-1);
+dp[0] = 0;
+
+for (let i = 0; i < n; i++) {
+    for (let j = 0; j < i; j++) {
+        if (dp[j] === -1) continue;
+
+        if (j + arr[j] >= i) {
+            dp[i] = Math.max(dp[i], dp[j] + 1)
+        }
+    }
+}
+
+console.log(Math.max(...dp))
