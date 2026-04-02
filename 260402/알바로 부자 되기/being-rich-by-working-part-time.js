0e69812e5f0a477cae6ffb91e8df54d36391e69b
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-04-02 15:43:13 +0900

    Title: 알바로 부자 되기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260402/알바로 부자 되기/being-rich-by-working-part-time.js
+++ 260402/알바로 부자 되기/being-rich-by-working-part-time.js
@@ -3,6 +3,7 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 const arr = input.slice(1).map(l => l.split(' ').map(Number));
 
+arr.sort((a,b)=>a[1]-b[1])
 // Please Write your code here.
 const dp = Array(n).fill(0);
 
@@ -10,13 +11,15 @@ for (let i = 0; i < n; i++) {
     dp[i] = arr[i][2];
 }
 
+// console.log(dp)
 for (let i = 0; i < n; i++) {
     for (let j = 0; j < i; j++) {
         if (arr[j][1] >= arr[i][0]) {
             dp[i] = Math.max(dp[i], dp[j])
         } else if (arr[j][1] < arr[i][0]) {
-            dp[i] = Math.max(dp[i], arr[i][2] + arr[j][2])
+            dp[i] = Math.max(dp[i], arr[i][2] + dp[j])
         }
+        // console.log(dp)
     }
 }
 
