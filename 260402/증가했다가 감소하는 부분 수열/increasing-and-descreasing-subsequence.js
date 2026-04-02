25a6a3ed8f8d1db5d2b5d8d664a684309f61642d
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-04-02 15:08:00 +0900

    Title: 증가했다가 감소하는 부분 수열, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260402/증가했다가 감소하는 부분 수열/increasing-and-descreasing-subsequence.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+const arr = input[1].split(' ').map(Number)
+
+const inc = Array(n).fill(1);
+const dec = Array(n).fill(1);
+
+for (let i = 0; i < n; i++) {
+    for (let j = 0; j < i; j++) {
+        if (arr[j] < arr[i]) {
+            inc[i] = Math.max(inc[i], inc[j] + 1)
+        }
+    }
+}
+
+for (let i = n-1; i >=0; i--) {
+    for (let j = i + 1; j < n; j++) {
+        if (arr[i] > arr[j]) {
+            dec[i] = Math.max(dec[i], dec[j] + 1)
+        }
+    }
+}
+
+console.log(inc);
+let ans = 0;
+for (let i = 0; i < n; i++) {
+    ans = Math.max(ans, inc[i] + dec[i] - 1)
+}
+
+console.log(ans);
