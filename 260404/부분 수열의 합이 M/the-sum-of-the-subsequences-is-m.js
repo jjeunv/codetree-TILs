afec88874df67903581b1fa9c5a734dd077b793f
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-04-04 01:06:04 +0900

    Title: 부분 수열의 합이 M, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260404/부분 수열의 합이 M/the-sum-of-the-subsequences-is-m.js
+++ 260404/부분 수열의 합이 M/the-sum-of-the-subsequences-is-m.js
@@ -8,13 +8,11 @@ let INT_MAX = Number.MAX_SAFE_INTEGER;
 
 const dp = Array(m + 1).fill(INT_MAX);
 dp[0] = 0;
-
-for (let i = m; i >=0; i--) {
-    for (let j = 0; j <n; j++) {
-        if (i >= arr[j]) {
-            dp[i] = Math.min(dp[i], dp[i - arr[j]] + 1)
+for(let i=0; i<n; i++){
+    for(let j=m; j>=0; j--){
+        if(j>=arr[i]){
+            dp[j] = Math.min(dp[j], dp[j-arr[i]]+1);
         }
     }
 }
-
-console.log(dp[m]===INT_MAX? -1 : dp[m]);
+console.log(dp[m] === INT_MAX ? -1 : dp[m]);
