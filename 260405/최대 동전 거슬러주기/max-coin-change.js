c67ae6bfcdc7dcf34999b71f23f9efe79ad0eef2
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-04-05 01:19:14 +0900

    Title: 최대 동전 거슬러주기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260405/최대 동전 거슬러주기/max-coin-change.js
+++ 260405/최대 동전 거슬러주기/max-coin-change.js
@@ -6,6 +6,7 @@ const dp = Array(m+1).fill(-1);
 dp[0] = 0
 for(let i=1; i<=m; i++){
     for(let j=0; j<n; j++){
+        if(dp[i-arr[j]]===-1) continue;
         if(i>=arr[j]){
         dp[i] = Math.max(dp[i], dp[i-arr[j]]+1);
         }
