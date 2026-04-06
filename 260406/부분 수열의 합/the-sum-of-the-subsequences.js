a8151b3cb7284cce34440a23a77f9b621f940ed3
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-04-06 22:58:53 +0900

    Title: 부분 수열의 합, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260406/부분 수열의 합/the-sum-of-the-subsequences.js
+++ 260406/부분 수열의 합/the-sum-of-the-subsequences.js
@@ -4,18 +4,32 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const [n, m] = input[0].split(' ').map(Number);
 const coin = input[1].split(' ').map(Number);
 
-let isTrue = false;
-// Please write your code here.
-function istheSumM(idx, sum) {
-    if (sum === m) {
-        isTrue = true;
-        return;
-    };
-    if (idx >= n) return;
+const dp = Array.from({ length: n }, () => Array(m + 1).fill(false));
 
-    istheSumM(idx + 1, sum + coin[idx]);
-    istheSumM(idx + 1, sum);
+for (let i = 0; i < n; i++) {
+    dp[i][0] = true;
 }
 
-istheSumM(0,0);
-console.log(isTrue ? "Yes" : "No")
+if (coin[0] <= m) {
+    dp[0][coin[0]] = true
+}
+
+
+function isTrue() {
+    if (dp[0][m]) return true
+    for (let i = 1; i < n; i++) {
+        for (let j = 1; j <= m; j++) {
+            dp[i][j] = dp[i - 1][j]
+
+            if (j >= coin[i]) {
+                dp[i][j] = dp[i][j] || dp[i - 1][j - coin[i]]
+            }
+        }
+        if (dp[i][m]) return true;
+    }
+}
+
+
+console.log(isTrue() ? "Yes" : "No")
+
+
