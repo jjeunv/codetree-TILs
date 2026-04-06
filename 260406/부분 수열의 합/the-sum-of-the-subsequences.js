830c87d08de763f87e45f04b9d2800f6dba89b9a
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-04-06 23:00:38 +0900

    Title: 부분 수열의 합, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260406/부분 수열의 합/the-sum-of-the-subsequences.js
+++ 260406/부분 수열의 합/the-sum-of-the-subsequences.js
@@ -4,20 +4,13 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const [n, m] = input[0].split(' ').map(Number);
 const coin = input[1].split(' ').map(Number);
 
-const dp = Array.from({ length: n }, () => Array(m + 1).fill(false));
+const dp = Array.from({ length: n + 1 }, () => Array(m + 1).fill(false));
 
-for (let i = 0; i < n; i++) {
-    dp[i][0] = true;
-}
-
-if (coin[0] <= m) {
-    dp[0][coin[0]] = true
-}
+dp[0][0] = true;
 
 
 function isTrue() {
-    if (dp[0][m]) return true
-    for (let i = 1; i < n; i++) {
+    for (let i = 1; i <= n; i++) {
         for (let j = 1; j <= m; j++) {
             dp[i][j] = dp[i - 1][j]
 
