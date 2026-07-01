98df6d0db01652423b8b992f1adc456a5f76bca7
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-07-01 20:11:19 +0900

    [Passed] 삽입 정렬 구현 (2ms, 1MB)

---
+++ 260701/삽입 정렬 구현/implement-insertion-sort.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int n;
+int arr[101];
+
+void insertion_sort(){
+    for(int i=1; i<n; i++){
+        int j=i-1;
+        int key=arr[i];
+        while(j>=0 && arr[j]>key){
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=key;
+    }
+}
+
+int main() {
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    insertion_sort();
+
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
