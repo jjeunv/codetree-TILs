9d0675ea3206670a88366bdbd8a067571752140b
jjeunv <124152742+jjeunv@users.noreply.github.com>
2026-07-14 23:40:05 +0900

    [Passed] 거품 정렬 구현 (2ms, 0MB)

---
+++ 260714/거품 정렬 구현/implement-bubble-sort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void bubbleSort(vector<int>& arr, int n){
+    bool sorted;
+    do{
+        sorted=true;
+        for(int i=0; i<n-1; i++){
+            if(arr[i]>arr[i+1]){
+                int tmp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1]=tmp;
+                sorted = false;
+            }
+        }
+    }while(!sorted);
+}
+int main(){
+    int n; cin>>n;
+    vector<int> arr(n);
+
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    bubbleSort(arr, n);
+
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+
+}
