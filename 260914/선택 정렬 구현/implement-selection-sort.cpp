a547ed614be0ac043081164517e69f0a51c248b1
진은비 <124152742+jjeunv@users.noreply.github.com>
2026-09-14 17:39:25 +0900

    [Passed] 선택 정렬 구현 (2ms, 0MB)

---
+++ 260914/선택 정렬 구현/implement-selection-sort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void selectionSort(int* arr, int n){
+    for(int i=0; i<n-1; i++){
+        int minIdx = i;
+        for(int j=i+1; j<n; j++){
+            if(arr[j]<arr[minIdx]){
+                minIdx = j;
+            }
+        }
+        int temp = arr[i];
+        arr[i] = arr[minIdx];
+        arr[minIdx] = temp;
+    }
+}
+
+int main(){
+    int arr[100];
+    int n;
+    cin>>n;
+
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    selectionSort(arr, n);
+
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
