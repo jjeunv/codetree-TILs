938b5b1d18ed27ae605fca7a161d6759cd122757
진은비 <124152742+jjeunv@users.noreply.github.com>
2026-09-24 03:32:12 +0900

    [Passed] 일의 자리 배열 (2ms, 0MB)

--- 260924/일의 자리 배열/array-with-units.cpp
+++ 260924/일의 자리 배열/array-with-units.cpp
@@ -2,14 +2,13 @@
 using namespace std;
 
 int main() {
-    int arr[10];
+
 
     int a;
     int b;
     cin >> a>> b;
 
-    arr[0]=a;
-    arr[1] =b;
+        int arr[10]={a,b};
 
     for(int i=2; i<10; i++){
         arr[i]=(arr[i-1]+arr[i-2])%10;
