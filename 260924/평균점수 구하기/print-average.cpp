ab9e34fcc4134dc3e9bf35c46f288eb9685911d4
진은비 <124152742+jjeunv@users.noreply.github.com>
2026-09-24 02:19:55 +0900

    [Passed] 평균점수 구하기 (2ms, 0MB)

---
+++ 260924/평균점수 구하기/print-average.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double arr[8];
+
+    for(int i=0; i<8; i++){
+        cin>>arr[i];
+    }
+
+    double sum = 0;
+    for(int i=0; i<8; i++){
+        sum+=arr[i];
+    }
+
+    cout<<fixed;
+    cout.precision(1);
+    cout<<sum/8;
+    return 0;
+}
