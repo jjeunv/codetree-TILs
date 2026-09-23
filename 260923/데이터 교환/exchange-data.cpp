7c4a43250f6190cdbedc936b5e90dea52cd2659d
진은비 <124152742+jjeunv@users.noreply.github.com>
2026-09-23 10:52:24 +0900

    [Passed] 데이터 교환 (2ms, 0MB)

---
+++ 260923/데이터 교환/exchange-data.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 6;
+    int c = 7;
+
+    int temp = a;
+
+    a = c;
+    c = b;
+    b = temp;
+
+    cout<< a<<endl<<b<<endl<<c;
+    return 0;
+}
